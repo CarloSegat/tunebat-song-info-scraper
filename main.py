798e6e96382c2e96a222851b31073c92b40c7780
carlosegat <segatcarlo95@gmail.com>
2023-01-14 14:22:07 +0100

    script to scrape stuff

---
+++ main.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+html_path = Path('./html/')
+song_path = Path('./songs/')
+
+def parse_html_file(file_path):
+    with open(file_path, 'r') as html_file:
+        html_content = html_file.read()
+        soup = BeautifulSoup(html_content, 'html5lib')
+        return soup
+
+def get_nice_song_name(soup):
+
+    def get_title():
+        h3s = soup.find_all('h3')
+        for h3 in h3s:
+             prev_node = h3.next_sibling
+             if prev_node.name == 'h1':
+                 return prev_node.string
+
+    def get_artist():
+        h3s = soup.find_all('h3')
+        for h3 in h3s:
+             prev_node = h3.next_sibling
+             if prev_node.name == 'h1':
+                 return h3.string
+
+
+    def find_secondary_prop_by_name(name):
+        secondary_spans = soup.find_all('span', class_="ant-typography-secondary")
+        for span in secondary_spans:
+            if span.string == name:
+                prev_node = span.previous_sibling
+                return prev_node.string
+
+    print(get_title())
+    print(get_artist())
+    print(find_secondary_prop_by_name('BPM'))
+    print(find_secondary_prop_by_name('camelot'))
+
+    new_song_name = f"{get_title().lower()}; {find_secondary_prop_by_name('BPM')}; {find_secondary_prop_by_name('camelot')}; {get_artist().lower()};"
+    return new_song_name
+
+
+for html_file in html_path.iterdir():
+    for song in song_path.iterdir():
+        if html_file.stem== song.stem:
+            html_full_name = html_path.joinpath(html_file.name)
+            soup = parse_html_file(html_full_name)
+            new_song_name = get_nice_song_name(soup) + song.suffix
+            print(f"> > > new_song_name{new_song_name}")
+            song_full_name = song_path.joinpath(song.name )
+            print(f"> > > song_full_name{song_full_name}")
+            song.rename(song_path.joinpath(new_song_name))
+
+
+
